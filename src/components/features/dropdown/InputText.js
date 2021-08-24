ca53f862baabba12fe5a9b82e9c8114161c748dc
Anonymous-Chris <sk.niyara@gmail.com>
2021-08-24 15:39:51 -0500

    add for text

--- src/components/features/dropdown/InputText.js
+++ src/components/features/dropdown/InputText.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
-const InputText = () => {
-  return <div>t</div>;
+const InputText = (props) => {
+  console.log(props);
+  const [inputValue, setInputValue] = useState("");
+
+  var changeInputValue = (e, name) => {
+    console.log(e.target.value, name);
+    setInputValue(e.target.value);
+  };
+  return (
+    <div
+      style={{
+        marginTop: "",
+        display: "flex",
+        height: "4vh",
+        fontSize: "1.3rem",
+      }}
+      className=""
+    >
+      <label className="">{"Input"}</label>
+      <input
+        type="text"
+        placeholder={"labelName"}
+        id={"eachItem.name"}
+        style={{
+          background: "inherit",
+          border: "1px solid black",
+          color: "grey",
+          paddingLeft: "5px",
+          marginLeft: "5px",
+        }}
+        value={inputValue}
+        onChange={(e) => changeInputValue(e, "name")}
+      ></input>
+    </div>
+  );
 };
 
 export default InputText;
