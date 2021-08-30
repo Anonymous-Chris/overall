3788c3bbc1499c82c8eecedecaca09edb6f41e55
Anonymous-Chris <sk.niyara@gmail.com>
2021-08-30 15:44:05 -0500

    add flickity

---
+++ src/components/features/Live.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../css/live.css";
+const Live = () => {
+  return (
+    <div className="liveStatus">
+      <div className="circle_outer">
+        <div className="circle_inner"></div>
+      </div>
+      <div> Live Data</div>
+    </div>
+  );
+};
+
+export default Live;
