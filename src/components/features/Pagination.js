8cf340385627510befcb638f78cd8a2e29eee230
Anonymous-Chris <sk.niyara@gmail.com>
2021-08-30 23:40:41 -0500

    update pagination logic

--- src/components/features/Pagination.js
+++ src/components/features/Pagination.js
@@ -1,10 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "../../css/pagination.css";
 
 const Pagination = () => {
   const [pageNumber, setpageNumber] = useState(1);
-  const totalPage = 10;
+  const [start, setStart] = useState(1);
+  const [end, setEnd] = useState(10);
+  const defaultNumber = 10;
+  const totalPage = 100;
+
+  useEffect(() => {
+    console.log(pageNumber);
+    var _start = defaultNumber * pageNumber - defaultNumber + 1;
+    var _end = defaultNumber * pageNumber;
+    setStart(_start);
+    setEnd(_end);
+  }, [pageNumber]);
   var decreasePage = () => {
     if (pageNumber > 1 && typeof pageNumber === "number") {
       setpageNumber(pageNumber - 1);
@@ -79,6 +90,10 @@ const Pagination = () => {
           onClick={gotoLast}
         ></i>
       </div>
+
+      <span>
+        Get data from {start} to {end}.
+      </span>
     </React.Fragment>
   );
 };
