b3d9fd9f9e6ae0d0b87c71a055ac903697c15cf1
Anonymous-Chris <sk.niyara@gmail.com>
2021-08-31 23:22:36 -0500

    update the css and logic for active button change

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/home.css";
+import { useTranslation } from "react-i18next";
 function Home() {
+  const [active, setActive] = useState(1);
+  const { i18n } = useTranslation();
+
+  const changeLanguage = (e, lng) => {
+    i18n.changeLanguage(lng);
+    setActive(e);
+  };
   return (
     <div className="home">
       <h1>Introduction</h1>
@@ -17,6 +25,27 @@ function Home() {
           feature.
         </p>
       </h5>
+      <h3>Localization (Change language from the following)</h3>
+      <div className="navbar_language">
+        <button
+          className={active === 1 ? "active" : ""}
+          onClick={(e) => changeLanguage(1, "en")}
+        >
+          en
+        </button>
+        <button
+          className={active === 2 ? "active" : ""}
+          onClick={(e) => changeLanguage(2, "np")}
+        >
+          np
+        </button>
+        <button
+          className={active === 3 ? "active" : ""}
+          onClick={(e) => changeLanguage(3, "ru")}
+        >
+          ru
+        </button>
+      </div>
     </div>
   );
 }
