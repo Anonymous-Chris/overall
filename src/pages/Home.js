e3ce2fb6b5c99d7d92485009ada37874bdbbcdf7
Anonymous-Chris <sk.niyara@gmail.com>
2021-08-31 22:41:01 -0500

    add localization packages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,8 +6,16 @@ function Home() {
       <h1>Introduction</h1>
       <h5>
         This project has few of the features I wanted to learn on the process
-        and which might be essential in future. Below is an implementation of
-        Localization feature.
+        and which might be essential in future. It also uses different
+        environment variables for test, dev and prod.
+        <p>
+          {" "}
+          Below is an implementation of
+          <b>
+            <i> Localization/Internalization</i>
+          </b>{" "}
+          feature.
+        </p>
       </h5>
     </div>
   );
