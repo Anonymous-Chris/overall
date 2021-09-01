81e7e3b27d2623bd64c73c707d580b962d8d126c
Anonymous-Chris <sk.niyara@gmail.com>
2021-09-01 00:23:42 -0500

    add html tags in json and convert using Trans component of i18n for localization

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../css/home.css";
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 function Home() {
   const { t, i18n } = useTranslation();
   console.log(i18n.language);
@@ -9,9 +9,16 @@ function Home() {
   };
   return (
     <div className="home">
-      <h1>Introduction</h1>
-      <h5>{t("home.introduction__text")}</h5>
-      <h3 data-i18n="[html]">{t("home.localization__title")}</h3>
+      <h1>{t("home.introduction__title")}</h1>
+      <h5>
+        <Trans
+          i18nKey="home.introduction__text"
+          components={{ b: <b />, i: <i />, p: <p /> }}
+        >
+          home.introduction__text
+        </Trans>
+      </h5>
+      <h3 style={{ marginTop: "5px" }}>{t("home.localization__title")}</h3>
       <div className="navbar_language">
         <button
           className={i18n.language === "en" ? "active" : ""}
