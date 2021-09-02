9f9c274872f00c8a673c77a6d0b22299754d8619
Anonymous-Chris <sk.niyara@gmail.com>
2021-09-02 00:01:19 -0500

    add world map image for heatmap

--- src/components/features/Heatmaps.js
+++ src/components/features/Heatmaps.js
@@ -1,7 +1,27 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "../../css/heatmaps.css";
 const Heatmaps = () => {
-  return <div>heatmaps</div>;
+  const [chosenValue, setChosenValue] = useState(0);
+  return (
+    <div className="heatmaps text-center w-100" style={{ height: "88vh" }}>
+      <h1>Heatmaps (Chosen : {chosenValue})</h1>
+      <div
+        className="col-12 col-sm-12 col-md-12 col-lg-8 m-auto"
+        style={{ height: "inherit" }}
+      >
+        <svg viewBox="0 0 160 100" style={{ height: "100%", width: "100%" }}>
+          <image
+            style={{ objectFit: "contain" }}
+            height="100%"
+            width="100%"
+            xlinkHref="https://cdn.shopify.com/s/files/1/0977/4104/products/mc-eur-305595-dark_2_2_911c0ac7-8c18-4c0d-a2ef-744eb234758b.png?v=1542948302"
+            alt="world"
+            // clipPath="url(#mask1)"
+          />
+        </svg>
+      </div>
+    </div>
+  );
 };
 
 export default Heatmaps;
---
+++ src/components/features/Mapping.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Mapping = () => {
+  return <div>Mapping</div>;
+};
+
+export default Mapping;
+Mapping;
