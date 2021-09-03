f2ebb81863596c74fe84a3d0ff3e1b115192789e
Anonymous-Chris <sk.niyara@gmail.com>
2021-09-03 00:32:00 -0500

    add interval

--- src/components/features/Heatmaps.js
+++ src/components/features/Heatmaps.js
@@ -1,14 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../../css/heatmaps.css";
 const Heatmaps = () => {
-  let [counter, setCounter] = useState(0);
-
-  setInterval(function () {
-    setCounter(counter + 1);
-  }, 10000);
+  let [counter, setcounter] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setcounter((counter) => counter + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className="heatmaps text-center w-100" style={{ height: "88vh" }}>
+      {console.log(counter)}
       <h1>Heatmaps (Chosen : {counter})</h1>
       <div
         className="col-12 col-sm-12 col-md-12 col-lg-8 m-auto"
@@ -23,6 +26,19 @@ const Heatmaps = () => {
             alt="world"
             // clipPath="url(#mask1)"
           />
+          {/* insert bounding boxes */}
+
+          <g key={1}>
+            <svg x={2} y={2} width="15" height="15">
+              <rect
+                className={` ${counter === 0}`}
+                width="15"
+                height="15"
+                fill="red"
+                fillOpacity={`${counter === 0 ? "0.8" : "0.5"}`}
+              ></rect>
+            </svg>
+          </g>
         </svg>
       </div>
     </div>
