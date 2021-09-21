2f1c2dfd2706271b81553d8cec86398479027b3b
Anonymous-Chris <sk.niyara@gmail.com>
2021-09-21 15:52:02 -0500

    add images

--- src/components/features/surveillance/Video.js
+++ src/components/features/surveillance/Video.js
@@ -6,16 +6,52 @@ const Video = () => {
       className="video w-100 h-100"
       style={{ border: "1px solid white", background: "#161619" }}
     >
-        <div className="row h-100 w-100">
-          <div className="col-12 col-sm-6 col-md-6 col-lg-4">1</div>
-          <div className="col-12 col-sm-6 col-md-6 col-lg-4">2</div>
-          <div className="col-12 col-sm-6 col-md-6 col-lg-4">3</div>
+      <div className="row h-100 w-100 m-0 p-1">
+        <div className="col-12 col-sm-6 col-md-6 col-lg-4 d-flex align-items-center justify-content-center" style={{border: '1px solid orange'}}>
+          <img
+            className="w-100"
+            src="https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg"
+            alt="image1"
+          />
+        </div>
+        <div className="col-12 col-sm-6 col-md-6 col-lg-4 d-flex align-items-center justify-content-center" style={{border: '1px solid orange'}}>
+          <img
+            className="w-100"
+            src="https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg"
+            alt="image1"
+          />
+        </div>
+        <div className="col-12 col-sm-6 col-md-6 col-lg-4 d-flex align-items-center justify-content-center" style={{border: '1px solid orange'}}>
+          <img
+            className="w-100"
+            src="https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg"
+            alt="image1"
+          />
+        </div>
         {/* </div>
         <div className="row h-50 w-100"> */}
-          <div className="col-12 col-sm-6 col-md-6 col-lg-4">1</div>
-          <div className="col-12 col-sm-6 col-md-6 col-lg-4">2</div>
-          <div className="col-12 col-sm-6 col-md-6 col-lg-4">3</div>
+        <div className="col-12 col-sm-6 col-md-6 col-lg-4 d-flex align-items-center justify-content-center" style={{border: '1px solid orange'}}>
+          <img
+            className="w-100"
+            src="https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg"
+            alt="image1"
+          />
         </div>
+        <div className="col-12 col-sm-6 col-md-6 col-lg-4 d-flex align-items-center justify-content-center" style={{border: '1px solid orange'}}>
+          <img
+            className="w-100"
+            src="https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg"
+            alt="image1"
+          />
+        </div>
+        <div className="col-12 col-sm-6 col-md-6 col-lg-4 d-flex align-items-center justify-content-center" style={{border: '1px solid orange'}}>
+          <img
+            className="w-100"
+            src="https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg"
+            alt="image1"
+          />
+        </div>
+      </div>
     </div>
   );
 };
