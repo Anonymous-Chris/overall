bd6bd5828392cc9f8125dae79090408de86ec0fe
Anonymous-Chris <sk.niyara@gmail.com>
2021-09-21 11:22:55 -0500

    add slider for surveillance overall

--- src/components/features/surveillance/Heatmaps.js
+++ src/components/features/surveillance/Heatmaps.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from "react";
 import fakedata from "../../../fakedata";
+import Slider from '../../functions/Slider'
 const Heatmaps = () => {
   const [data, setData] = useState({});
   useEffect(() => {
     setData(fakedata[0].surveillanceoverall);
+    Slider()
+
   }, []);
+
+
   return (
     <div className="heatmaps h-100 pt-2" style={{ background: "#161619" }}>
       <span>
@@ -13,7 +18,7 @@ const Heatmaps = () => {
       <div className="scrollbar w-100" style={{ height: "84%" }}>
         {data && (
           <ul
-            className="m-0 p-0 d-flex w-100 h-75"
+            className="m-0 p-0 d-flex w-100 h-75 camera-list"
             style={{ overflow: "scroll" }}
           >
             {data[0]?.data.map((singleLog, index) => (
