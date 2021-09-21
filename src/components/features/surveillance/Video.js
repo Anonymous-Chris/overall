25a50b2e066191f5f21e8d3f82912bc833fcb12f
Anonymous-Chris <sk.niyara@gmail.com>
2021-09-21 14:46:03 -0500

    start video boxes

--- src/components/features/surveillance/Video.js
+++ src/components/features/surveillance/Video.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Video = () => {
-  return <div className="video w-100 h-100" style={{border: '1px solid white'}}>video</div>;
+  return (
+    <div
+      className="video w-100 h-100"
+      style={{ border: "1px solid white", background: "#161619" }}
+    >
+        <div className="row h-100 w-100">
+          <div className="col-12 col-sm-6 col-md-6 col-lg-4">1</div>
+          <div className="col-12 col-sm-6 col-md-6 col-lg-4">2</div>
+          <div className="col-12 col-sm-6 col-md-6 col-lg-4">3</div>
+        {/* </div>
+        <div className="row h-50 w-100"> */}
+          <div className="col-12 col-sm-6 col-md-6 col-lg-4">1</div>
+          <div className="col-12 col-sm-6 col-md-6 col-lg-4">2</div>
+          <div className="col-12 col-sm-6 col-md-6 col-lg-4">3</div>
+        </div>
+    </div>
+  );
 };
 
 export default Video;
