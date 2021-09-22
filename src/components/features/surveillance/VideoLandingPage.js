b0e3fc5131358856a21c5f3956c08220c1868b1f
Anonymous-Chris <sk.niyara@gmail.com>
2021-09-22 11:53:50 -0500

    here till commit in git

---
+++ src/components/features/surveillance/VideoLandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const VideoLandingPage = () => {
+    return (
+        <div>
+            video landing page
+        </div>
+    )
+}
+
+export default VideoLandingPage
