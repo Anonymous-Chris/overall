c3fee4e5647c8b355297cc3eb2f2ce98dea66dc7
Anonymous-Chris <sk.niyara@gmail.com>
2021-09-22 10:36:15 -0500

    add expand icon

--- src/components/features/surveillance/Video.js
+++ src/components/features/surveillance/Video.js
@@ -64,46 +64,86 @@ const Video = () => {
           </FullScreen>
         </div>
         <div
-          className="col-12 col-sm-6 col-md-6 col-lg-4 d-flex align-items-center justify-content-center"
+          className="col-12 col-sm-6 col-md-6 col-lg-4 d-flex align-items-center justify-content-center position-relative "
           style={{ border: "1px solid orange" }}
         >
-          <img
-            className="w-100"
-            src="https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg"
-            alt="image1"
-          />
+          <FullScreen handle={handle1} className="w-100 h-100 d-flex align-items-center justify-content-center">
+            <img
+              className="w-100"
+              src="https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg"
+              alt="image1"
+            />
+            <div className="w-100 d-flex position-absolute p-1 bottom-0 start-0 responsive__bar">
+              <div className="livebtn  bottom-0 start-0 p-1">live</div>
+              <div className="camera p-1">camera 1</div>
+              <i
+                className="fas fa-expand fa-2x"
+                onClick={handleExpand(handle1)}
+              ></i>
+            </div>
+          </FullScreen>
         </div>
         {/* </div>
         <div className="row h-50 w-100"> */}
         <div
-          className="col-12 col-sm-6 col-md-6 col-lg-4 d-flex align-items-center justify-content-center"
+          className="col-12 col-sm-6 col-md-6 col-lg-4 d-flex align-items-center justify-content-center position-relative"
           style={{ border: "1px solid orange" }}
         >
-          <img
-            className="w-100"
-            src="https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg"
-            alt="image1"
-          />
+          <FullScreen handle={handle1} className="w-100 h-100 d-flex align-items-center justify-content-center">
+            <img
+              className="w-100"
+              src="https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg"
+              alt="image1"
+            />
+            <div className="w-100 d-flex position-absolute p-1 bottom-0 start-0 responsive__bar">
+              <div className="livebtn  bottom-0 start-0 p-1">live</div>
+              <div className="camera p-1">camera 1</div>
+              <i
+                className="fas fa-expand fa-2x"
+                onClick={handleExpand(handle1)}
+              ></i>
+            </div>
+          </FullScreen>
         </div>
         <div
-          className="col-12 col-sm-6 col-md-6 col-lg-4 d-flex align-items-center justify-content-center"
+          className="col-12 col-sm-6 col-md-6 col-lg-4 d-flex align-items-center justify-content-center position-relative"
           style={{ border: "1px solid orange" }}
         >
-          <img
-            className="w-100"
-            src="https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg"
-            alt="image1"
-          />
+          <FullScreen handle={handle1} className="w-100 h-100 d-flex align-items-center justify-content-center">
+            <img
+              className="w-100"
+              src="https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg"
+              alt="image1"
+            />
+            <div className="w-100 d-flex position-absolute p-1 bottom-0 start-0 responsive__bar">
+              <div className="livebtn  bottom-0 start-0 p-1">live</div>
+              <div className="camera p-1">camera 1</div>
+              <i
+                className="fas fa-expand fa-2x"
+                onClick={handleExpand(handle1)}
+              ></i>
+            </div>
+          </FullScreen>
         </div>
         <div
-          className="col-12 col-sm-6 col-md-6 col-lg-4 d-flex align-items-center justify-content-center"
+          className="col-12 col-sm-6 col-md-6 col-lg-4 d-flex align-items-center justify-content-center position-relative"
           style={{ border: "1px solid orange" }}
         >
-          <img
-            className="w-100"
-            src="https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg"
-            alt="image1"
-          />
+          <FullScreen handle={handle1} className="w-100 h-100 d-flex align-items-center justify-content-center">
+            <img
+              className="w-100"
+              src="https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg"
+              alt="image1"
+            />
+            <div className="w-100 d-flex position-absolute p-1 bottom-0 start-0 responsive__bar">
+              <div className="livebtn  bottom-0 start-0 p-1">live</div>
+              <div className="camera p-1">camera 1</div>
+              <i
+                className="fas fa-expand fa-2x"
+                onClick={handleExpand(handle1)}
+              ></i>
+            </div>
+          </FullScreen>
         </div>
       </div>
     </div>
