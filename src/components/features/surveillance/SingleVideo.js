0de20c848219f7481318cbb393596cda43d113cd
Anonymous-Chris <sk.niyara@gmail.com>
2021-09-23 13:18:58 -0500

    Add video component

--- src/components/features/surveillance/SingleVideo.js
+++ src/components/features/surveillance/SingleVideo.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+// import { FullScreen, useFullScreenHandle } from "react-full-screen";
 
 const SingleVideo = () => {
-    return (
-        <div className="singlevideo">
-            singlevideo
-        </div>
-    )
-}
+  return (
+    <div
+      className="singlevideo col-12 col-sm-6 col-md-6 col-lg-4 d-flex align-items-center justify-content-center position-relative "
+      style={{ border: "1px solid orange" }}
+    >
+      singlevideo
+    </div>
+  );
+};
 
-export default SingleVideo
+export default SingleVideo;
--- src/components/features/surveillance/VideoLandingPage.js
+++ src/components/features/surveillance/VideoLandingPage.js
@@ -1,20 +1,35 @@
-import React, {useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useStateValue } from "../../../context-api/StateProvider";
-
+import SingleVideo from "./SingleVideo";
 const VideoLandingPage = () => {
   const [{ cameraStatus }] = useStateValue();
-  const [camera_list,setCameraList] =useState([])
+  const [camera_list, setCameraList] = useState([]);
+
   useEffect(() => {
     try {
       var camera_list = JSON.parse(cameraStatus);
-    //   console.log(camera_list, 'here');
-      setCameraList(camera_list)
+      //   console.log(camera_list, 'here');
+      setCameraList(camera_list);
     } catch (e) {
       console.log("error");
     }
   }, [cameraStatus]);
-  return <div>video landing page
-{console.log(camera_list,'here')}  </div>;
+
+  const returnCameraComponent = (camera_list) => {
+    if (camera_list.length > 0) {
+      console.log(camera_list);
+      var individualComponent = camera_list.map((item) =>
+        item.state ? <SingleVideo key={item.name} /> : ""
+      );
+      return individualComponent;
+    }
+  };
+
+  return (
+    <div className="row h-100 w-100 m-0 p-1">
+      {returnCameraComponent(camera_list)}
+    </div>
+  );
 };
 
 export default VideoLandingPage;
