3d0e1961b819df55c5a4e2f3584596f5e64be3d4
Anonymous-Chris <sk.niyara@gmail.com>
2021-09-30 13:38:42 -0500

    set 100% height to the donut

--- src/components/features/surveillance/OverallPeople.js
+++ src/components/features/surveillance/OverallPeople.js
@@ -22,7 +22,7 @@ const OverallPeople = () => {
       {/* <h4>{peopleDetected[0]?.metadata.name}</h4> */}
       <div
         className="d-flex align-items-center justify-content-center w-100"
-        style={{ height: "87%" }}
+        style={{ height: "100%" }}
       >
         <Donut
           title={peopleDetected[0]?.metadata.name}
