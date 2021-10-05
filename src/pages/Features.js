720dcc13c008b0040ee9883b16b0831668533880
Anonymous-Chris <sk.niyara@gmail.com>
2021-10-04 21:54:49 -0500

    add replace in Link to avoid warnings

--- src/pages/Features.js
+++ src/pages/Features.js
@@ -16,51 +16,51 @@ function Features() {
       <div className="row w-100 m-0 p-1 h-31 " style={{ height: "inherit" }}>
         {/* first 3 */}
         <div className="col-sm-12 col-md-6 col-lg-4 features__item">
-          <Link to="/features/dynamictable">
+          <Link to="/features/dynamictable" replace>
             <img src={dynamictable} className="w-100 h-100" alt="first" />
           </Link>
         </div>
         <div className="col-sm-12 col-md-6 col-lg-4 features__item ">
-          <Link to="/features/autoscroll">
+          <Link to="/features/autoscroll" replace>
             <img src={autoscroll} className="w-100 h-100" alt="first" />
           </Link>
         </div>
         <div className="col-sm-12 col-md-6 col-lg-4 features__item ">
-          <Link to="/features/nesteddonut">
+          <Link to="/features/nesteddonut" replace>
             <img src={donuts} className="w-100 h-100" alt="first" />
           </Link>
         </div>
         {/* second 3 */}
 
         <div className="col-sm-12 col-md-6 col-lg-4 features__item">
-          <Link to="/features/loader">
+          <Link to="/features/loader" replace>
             <img src={loader} className="w-100 h-100" alt="first" />
           </Link>
         </div>
         <div className="col-sm-12 col-md-6 col-lg-4 features__item ">
-          <Link to="/features/dropdowns">
+          <Link to="/features/dropdowns" replace>
             <img src={dropdowns} className="w-100 h-100" alt="first" />
           </Link>
         </div>
         <div className="col-sm-12 col-md-6 col-lg-4 features__item ">
           <Link to="/features/datepicker">
-            <img src={contextapi} className="w-100 h-100" alt="first" />
+            <img src={contextapi} className="w-100 h-100" alt="first replace" />
           </Link>
         </div>
         {/* third 3 */}
 
         <div className="col-sm-12 col-md-6 col-lg-4 features__item">
-          <Link to="/features/live">
+          <Link to="/features/live " replace>
             <img src={live} className="w-100 h-100" alt="first" />
           </Link>
         </div>
         <div className="col-sm-12 col-md-6 col-lg-4 features__item ">
-          <Link to="/features/flickity">
+          <Link to="/features/flickity" replace>
             <img src={flickity} className="w-100 h-100" alt="first" />
           </Link>
         </div>
         <div className="col-sm-12 col-md-6 col-lg-4 features__item ">
-          <Link to="/features/pagination">
+          <Link to="/features/pagination" replace> 
             <img
               src={skipandfetchpagination}
               className="w-100 h-100"
