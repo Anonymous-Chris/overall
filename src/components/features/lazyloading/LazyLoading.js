f55d4d3eb5411d203346336050f68f535bd50389
Anonymous-Chris <sk.niyara@gmail.com>
2021-10-05 09:28:20 -0500

    add lazyloading to the images

--- src/components/features/lazyloading/LazyLoading.js
+++ src/components/features/lazyloading/LazyLoading.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import styled from "styled-components";
+import LazyImage from "./LazyImage";
+
+const Grid = styled.div`
+  display: grid;
+  padding: 16px;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 16px;
+  grid-auto-rows: minmax(48vh, auto);
+
+`;
 
 const LazyLoading = () => {
     return (
-        <div>
-            lazy loading
-        </div>
+        <Grid>
+        {[...Array(50).keys()].map(i => (
+          <LazyImage
+            key={i}
+            src={`https://picsum.photos/932/455?random=${i}`}
+            alt={`Random image ${i}`}
+          />
+        ))}
+      </Grid>
     )
 }
 
