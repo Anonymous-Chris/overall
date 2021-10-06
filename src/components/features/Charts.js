06534596af4019e1c9862ea71dedf92022b0bb33
Anonymous-Chris <sk.niyara@gmail.com>
2021-10-05 19:08:02 -0500

    add packages for charts

--- src/components/features/Charts.js
+++ src/components/features/Charts.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Charts = () => {
-  return <div>Charts</div>;
+  return <div className="charts bg-dark w-100 h-100 text-white">Charts</div>;
 };
 
 export default Charts;
