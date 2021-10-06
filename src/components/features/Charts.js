296a75f77cf8b07be6ad4a363a53e579af5b40e6
Anonymous-Chris <sk.niyara@gmail.com>
2021-10-05 19:58:21 -0500

    layout

--- src/components/features/Charts.js
+++ src/components/features/Charts.js
@@ -1,7 +1,52 @@
 import React from "react";
 
 const Charts = () => {
-  return <div className="charts bg-dark w-100 h-100 text-white">Charts</div>;
+  return (
+    <div
+      className="charts w-100 h-100 text-white text-center"
+      style={{ background: "#161619" }}
+    >
+      <h5>Charts</h5>
+      <div className="row m-0 p-0" style={{ background: "#161619" }}>
+        <div
+          className="col-12 col-sm-12 col-md-6 col-lg-6 p-1 "
+          style={{ border: "1px solid orange", background: "#212224" }}
+        >
+          rgb(33, 34, 36){" "}
+        </div>
+        <div
+          className="col-12 col-sm-12 col-md-6 col-lg-6 p-1 "
+          style={{ border: "1px solid orange", background: "#212224" }}
+        >
+          two{" "}
+        </div>
+        <div
+          className="col-12 col-sm-12 col-md-6 col-lg-6 p-1  "
+          style={{ border: "1px solid orange", background: "#212224" }}
+        >
+          three{" "}
+        </div>
+        <div
+          className="col-12 col-sm-12 col-md-6 col-lg-6 p-1 "
+          style={{ border: "1px solid orange", background: "#212224" }}
+        >
+          four{" "}
+        </div>
+        <div
+          className="col-12 col-sm-12 col-md-6 col-lg-6 p-1 "
+          style={{ border: "1px solid orange", background: "#212224" }}
+        >
+          five{" "}
+        </div>
+        <div
+          className="col-12 col-sm-12 col-md-6 col-lg-6 p-1 "
+          style={{ border: "1px solid orange", background: "#212224" }}
+        >
+          six{" "}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Charts;
